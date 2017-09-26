9844ab17028c65af3f1df3b32923b9df50a11eae
Joao Pedro Zanchett <joaozanchett2@gmail.com>
2017-09-26 16:48:51 -0300

    Praticamente pronto 2/3

--- client/components/DespesaCard.jsx
+++ client/components/DespesaCard.jsx
@@ -8,7 +8,7 @@ export default React.createClass({
         <div className="row">
           <div className="col s12">
             {Carteira.lancamentos.map((valores) =>
-              <div className="card blue-white darken-1 col s6" key={valores.motivo}>
+              <div className="card blue-white darken-1 col s12" key={valores.motivo}>
                 <div className="card-content white-text">
                   <span className="card-title blue-text"><b>{valores.motivo}</b></span>
                   <h5 className="blue-text"><b>Horário: </b>{valores.hora+"hs" }</h5>
